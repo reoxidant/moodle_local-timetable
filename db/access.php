a0f56bd02c4eee23ddafba01aa7cd7e34f3cbfec
Виталий Шаповалов <vitalijsapovalov@MacBook-Pro-Vitalij.local>
2020-06-09 13:53:16 +0300

    [EDIT] reformat code

--- db/access.php
+++ db/access.php
@@ -3,11 +3,11 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
-	'local/student_timetable:view' => array(
+    'local/student_timetable:view' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'manager' => CAP_ALLOW
-		),
+        ),
     ),
 );
